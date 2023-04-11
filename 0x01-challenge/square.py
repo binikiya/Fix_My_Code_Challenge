ddefa9508f640cb7765eb26ddf2f5f0270136bd4
Biniyam <biniyamkiya3682@gmail.com>
2023-04-11 15:11:52 +0300

    Square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Square class midel"""
+""" Square class midel definition """
 
 
 class Square():
-    """square class"""
+    """ Square class """
     width = 0
     height = 0
     
     def __init__(self, *args, **kwargs):
-        """the start of Square class"""
+        """ The start of Square class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -17,14 +17,14 @@ class Square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
-        """ Perimeter of square class"""
+        """ Perimeter of square class """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-    """main function of Square class"""
+    """ Main function of Square class """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
