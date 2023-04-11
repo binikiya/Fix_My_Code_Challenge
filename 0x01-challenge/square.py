0bd38bce1c540549d8e8e08f269ff5a9954f0493
Biniyam <biniyamkiya3682@gmail.com>
2023-04-11 15:20:29 +0300

    Square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class Square():
         """ Area of the Square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """ Perimeter of square class """
         return (self.width * 2) + (self.height * 2)
 
@@ -29,4 +29,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
